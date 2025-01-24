762ab45e2dbdc63d96c87358a1317e6f1dd05761
Akzhan Mataev <akjanmataev@gmail.com>
2025-01-24 15:32:14 +0500

    Updated models in models.py
    - Added new Pydantic models for input validation
    - Modified existing models to better match the database structure
    - Ensured proper typing and validation for all model fields

--- src/models.py
+++ src/models.py
@@ -1,5 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from bson import ObjectId
+
+class PostInputModel(BaseModel):
+    title: str
+    content: str
 
 class PostModel(BaseModel):
     author: str
@@ -8,11 +13,25 @@ class PostModel(BaseModel):
     created_at: datetime = datetime.now()
     updated_at: datetime = datetime.now()
 
+class PostOutPutModel(BaseModel):
+    _id : ObjectId
+    author: str
+    title: str
+    content: str
+    created_at: datetime 
+    updated_at: datetime 
+
 class UpdateInputModel(BaseModel):
     title : str
     content : str
 
-class UpdateModel(BaseModel):
-    title: str
-    content: str
-    updated_at: datetime = datetime.now()
+class RegistrationInputModel(BaseModel) :
+    email : EmailStr
+    username : str
+    password : str
+
+
+
+class LoginInputModel(BaseModel):
+    email : EmailStr
+    password : str
